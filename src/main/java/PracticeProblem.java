08dae78640a988342c94e9f9299c4fc5c339c510
Joe-Yang1 <348993072@tdsb.ca>
2026-03-03 16:15:57 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,11 @@
+/**
+	* File: Lesson 1.11 - Math
+	* Author: Joe Yang
+	* Date Created: Feb 24, 2023
+	* Date Last Modified: Feb 24, 2023
+	*/
+
+import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -6,22 +14,66 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a number: ");
+	double num;
+	num = input.nextDouble();
+	System.out.println(Math.abs(num));
+		
+	
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a number: ");
+	double num;
+	num = input.nextDouble();
+	System.out.print("Input another number: ");
+	double num2;
+	num2 = input.nextDouble();
+	System.out.println(Math.floor(num/num2));
+	System.out.println(Math.ceil(num/num2));
+	
+
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a number: ");
+	double num;
+	num = input.nextDouble();
+	System.out.println((int)(Math.floor(Math.sqrt(num))));
+
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a number: ");
+	double num;
+	num = input.nextDouble();
+	System.out.print("Input another number: ");
+	double num2;
+	num2 = input.nextDouble();
+	System.out.println(Math.pow(num, num2));
 	}
 
 	public static void q5() {
 		//Write question 5 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a number: ");
+	double num;
+	num = input.nextDouble();
+	System.out.print("Input another number: ");
+	double num2;
+	num2 = input.nextDouble();
+	System.out.print("Input one more number: ");
+	double num3;
+	num3 = input.nextDouble();
+	System.out.println(Math.max(Math.max(num, num2), num3));
+	System.out.println(Math.min(Math.min(num, num2), num3));
 	}
 
 }
